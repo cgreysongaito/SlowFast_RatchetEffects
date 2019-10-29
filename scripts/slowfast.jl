1507ba401ba99445dcce30132d7753979754441c
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2019-10-29 15:55:45 -0400

    added structuring to slowfast

--- scripts/slowfast.jl
+++ scripts/slowfast.jl
@@ -2,6 +2,48 @@
 
 include("packages.jl")
 
+#Non-dimensionalized
+@with_kw mutable struct RozMacPar
+    r = 2.0
+    K = 3.0
+    a = 1.1
+    h = 0.8
+    e = 0.7
+    m = 0.4
+    σ = 0.1
+    ε = 0.1
+end
+
+function roz_mac_II!(du, u, p, t,)
+    @unpack r, K, a, h, e, m = p
+    R, C = u
+    du[1] = r * R * (1 - R / K) - a * R * C / (1 + a * h * R)
+    du[2] = ε ( e * a * R * C / (1 + a * h * R) - m * C )
+    return
+end
+
+function roz_mac_II(u, par)
+    du = similar(u)
+    roz_mac_II!(du, u, par, 0.0)
+    return du
+end
+
+# Find equilibria - should be same as normal but also with ε = 0
+
+# Find isoclines
+
+# Create vector fields
+# - Before hopf fixed point
+
+# - Before hopf damped oscillations
+
+# - At hopf
+
+# - After hopf (limit cycle)
+
+
+## Dimensionalized
+#Setup
 @with_kw mutable struct RozMacPar
     r = 2.0
     K = 3.0
@@ -20,8 +62,15 @@ function roz_mac_II!(du, u, p, t,)
     return
 end
 
-function roz_mac_II(u, par)
-    du = similar(u)
-    roz_mac_II!(du, u, par, 0.0)
-    return du
-end
+# Find equilibria - should be same as normal but also with ε = 0
+
+# Find isoclines
+
+# Create vector fields
+# - Before hopf fixed point
+
+# - Before hopf damped oscillations
+
+# - At hopf
+
+# - After hopf (limit cycle)
