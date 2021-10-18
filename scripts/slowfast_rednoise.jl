626c9ed78392aec686ff79434fc699999f6e114b
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2021-10-18 19:09:17 -0400

    fixed paths in rednoise

--- scripts/slowfast_rednoise.jl
+++ scripts/slowfast_rednoise.jl
@@ -2,9 +2,10 @@ using .Threads
 using CSV
 using DataFrames
 
-include("/home/chrisgg/julia/TimeDelays/scripts/packages.jl")
-include("/home/chrisgg/julia/TimeDelays/scripts/slowfast_commoncode.jl")
-include("/home/chrisgg/julia/TimeDelays/scripts/slowfast_canardfinder.jl")
+include("packages.jl")
+include("slowfast_commoncode.jl")
+include("slowfast_metabolicmodel.jl")
+include("slowfast_canardfinder.jl")
 
 function prop_canard_rednoise(model, ep, effR0, r, reps)
     count_canard = 0
@@ -36,38 +37,38 @@ end
 
 ### RozMac model
 begin
-    rn_ep0079eff05_RozMac, = prop_canard_rednoise_data("RozMac", 0.079, 0.5, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff05_RozMac.csv", rn_ep0079eff05_RozMac)
+    rn_ep0079eff05_RozMac = prop_canard_rednoise_data("RozMac", 0.079, 0.5, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep0079eff05_RozMac.csv"), rn_ep0079eff05_RozMac)
     rn_ep0079eff06_RozMac = prop_canard_rednoise_data("RozMac",0.079, 0.6, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff06_RozMac.csv", rn_ep0079eff06_RozMac)
+    CSV.write(joinpath(abpath(),"data/rn_ep0079eff06_RozMac.csv"), rn_ep0079eff06_RozMac)
     rn_ep0079eff07_RozMac = prop_canard_rednoise_data("RozMac",0.079, 0.7, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff07_RozMac.csv", rn_ep0079eff07_RozMac)
+    CSV.write(joinpath(abpath(), "data/rn_ep0079eff07_RozMac.csv"), rn_ep0079eff07_RozMac)
 end
 
 begin
     rn_ep0004eff05_RozMac = prop_canard_rednoise_data("RozMac",0.004, 0.5, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff05_RozMac.csv", rn_ep0004eff05_RozMac)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff05_RozMac.csv"), rn_ep0004eff05_RozMac)
     rn_ep0004eff06_RozMac = prop_canard_rednoise_data("RozMac",0.004, 0.6, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff06_RozMac.csv", rn_ep0004eff06_RozMac)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff06_RozMac.csv"), rn_ep0004eff06_RozMac)
     rn_ep0004eff07_RozMac = prop_canard_rednoise_data("RozMac",0.004, 0.7, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff07_RozMac.csv", rn_ep0004eff07_RozMac)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff07_RozMac.csv"), rn_ep0004eff07_RozMac)
 end
 
 ### YodInn model
 begin
-    rn_ep0079eff05_YodInn = prop_canard_rednoise_data("YodInn", 0.079, 0.5, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff05_YodInn.csv", rn_ep0079eff05_YodInn)
-    rn_ep0079eff06_YodInn = prop_canard_rednoise_data("YodInn", 0.079, 0.6, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff06_YodInn.csv", rn_ep0079eff06_YodInn)
-    rn_ep0079eff07_YodInn = prop_canard_rednoise_data("YodInn", 0.079, 0.7, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0079eff07_YodInn.csv", rn_ep0079eff07_YodInn)
+    rn_ep00079eff12_YodInn = prop_canard_rednoise_data("YodInn", 0.0079, 1.2, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep00079eff12_YodInn.csv"), rn_ep00079eff12_YodInn)
+    rn_ep00079eff10_YodInn = prop_canard_rednoise_data("YodInn", 0.0079, 1.0, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep00079eff10_YodInn.csv"), rn_ep00079eff10_YodInn)
+    rn_ep00079eff08_YodInn = prop_canard_rednoise_data("YodInn", 0.079, 0.8, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep00079eff08_YodInn.csv"), rn_ep00079eff08_YodInn)
 end
 
 begin
-    rn_ep0004eff05_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 0.5, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff05_YodInn.csv", rn_ep0004eff05_YodInn)
-    rn_ep0004eff06_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 0.6, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff06_YodInn.csv", rn_ep0004eff06_YodInn)
-    rn_ep0004eff07_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 0.7, 1000)
-    CSV.write("/home/chrisgg/julia/TimeDelays/data/rn_ep0004eff07_YodInn.csv", rn_ep0004eff07_YodInn)
+    rn_ep0004eff12_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 1.2, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff12_YodInn.csv"), rn_ep0004eff12_YodInn)
+    rn_ep0004eff10_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 1.0, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff10_YodInn.csv"), rn_ep0004eff10_YodInn)
+    rn_ep0004eff08_YodInn = prop_canard_rednoise_data("YodInn", 0.004, 0.8, 1000)
+    CSV.write(joinpath(abpath(), "data/rn_ep0004eff08_YodInn.csv"), rn_ep0004eff08_YodInn)
 end
