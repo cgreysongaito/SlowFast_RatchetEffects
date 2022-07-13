10f302444651469e6b872fc80f339ef7badf75cb
chrisgg <chris@greyson-gaito.co.uk>
2022-07-13 16:30:13 -0400

    updating figures

--- scripts/slowfast_commoncode.jl
+++ scripts/slowfast_commoncode.jl
@@ -201,13 +201,13 @@ function findRCdivide_epx_data()
 end # Function to calculate the proportion of real in efficiency "parameter space" for multiple values of ε.
 
 
-function quasicycle_data(ep, reps)
+function quasicycle_data(ep, eff, reps)
     lrange = 0:1:40
     ACFdata = Vector{Vector{Float64}}(undef,reps)
     avprep = zeros(reps)
     avfinal = zeros(length(lrange))
     @threads for i in 1:reps
-        sol = RozMac_pert(ep, 0.6, 1.0, 0.0, i, 10000.0, 9000.0:1.0:10000.0)
+        sol = RozMac_pert(ep, eff, 1.0, 0.0, i, 10000.0, 9000.0:1.0:10000.0)
         @inbounds ACFdata[i] = autocor(sol[2, 1:end], collect(lrange))
     end
     for j in 1:length(lrange)
